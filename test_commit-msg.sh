a4ed1daf51810e76f98013aab1f1ca7171b40939
murank <murank42@gmail.com>
2011-09-26 22:46:45 +0900

    Save/restore config in setup/teardown of test_commitmsg.sh

--- test_commit-msg.sh
+++ test_commit-msg.sh
@@ -1,12 +1,39 @@
 #! /bin/sh
 
+saveconfig()
+{
+    local key="$1"
+    local conffile="$key.tmp"
+
+    git config --local "$key" > "$conffile"
+}
+
+restoreconfig()
+{
+    local key="$1"
+    local conffile=$key.tmp
+
+    if [ -s $conffile ]; then
+        echo "git config  --local '$key' '$(cat $conffile)'" | sh
+    else
+        git config --local --unset "$key"
+    fi
+    rm $conffile
+}
+
 setup()
 {
     git commit -a -m "tmp commit for test_commit-msg.sh. PID $$" >/dev/null 2>&1
+
+    saveconfig hook.topicBranchFormat
+    saveconfig hook.msg4TopicBranch
 }
 
 teardown()
 {
+    restoreconfig hook.msg4TopicBranch
+    restoreconfig hook.topicBranchFormat
+
     git log -1 --pretty=format:"%s" | grep "tmp commit for test_commit-msg.sh. PID $$" >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         git reset HEAD^ >/dev/null 2>&1
