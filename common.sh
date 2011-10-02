a61e34ff7f8573e088604c0a9419885901042817
murank <murank42@gmail.com>
2011-10-02 13:40:51 +0900

    Fix a bug of getting a branch name

--- common.sh
+++ common.sh
@@ -2,8 +2,7 @@
 
 getGitBranchName()
 {
-    branch="$(git symbolic-ref HEAD 2>/dev/null)" ||
-            "$(git describe --contains --all HEAD)"
+    branch="$(git symbolic-ref HEAD 2>/dev/null || git describe --contains --all HEAD)"
     echo ${branch##refs/heads/}
 }
 
